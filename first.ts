4788b8ce10a78f85386af63c039a1e46dd38ba62
sheikhemon <sheikhemon@gmail.com>
2021-09-13 01:01:38 +0600

    strcit tsc type

--- first.ts
+++ first.ts
@@ -1,2 +1,29 @@
 const num = 5;
 console.log('Hlw type script', num);
+
+/* Strict type */
+let first: number = 5;
+let second: string = 'This is type of string';
+let third: boolean = true;
+
+/*
+    Set function parameter type and function return type
+*/
+function add(first: number, second: number): number {
+    const result = first + second;
+    return result;
+}
+const output: number = add(45, 47);
+console.log(output);
+// console func
+function doubleConsole(number: number): void {
+    console.log(2 * number);
+}
+
+// Multi purpose
+/* function add(first: number | string, second: number | string): number | string {
+    const result: string | number = first + second;
+    return result;
+}
+const output: number = add(45, 47);
+console.log(output); */
--- js/first.js
+++ js/first.js
@@ -1,3 +1,27 @@
 "use strict";
 const num = 5;
 console.log('Hlw type script', num);
+/* Strict type */
+let first = 5;
+let second = 'This is type of string';
+let third = true;
+/*
+    Set function parameter type and function return type
+*/
+function add(first, second) {
+    const result = first + second;
+    return result;
+}
+const output = add(45, 47);
+console.log(output);
+// console func
+function doubleConsole(number) {
+    console.log(2 * number);
+}
+// Multi purpose
+/* function add(first: number | string, second: number | string): number | string {
+    const result: string | number = first + second;
+    return result;
+}
+const output: number = add(45, 47);
+console.log(output); */ 
